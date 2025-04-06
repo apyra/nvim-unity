7ea58dd86dbd845c43b47aede5c16c46e8e3d4b1
apyra <apyra@outlook.com.br>
2025-04-06 16:13:57 -0300

    Bug fixes

--- Editor/NeovimCodeEditor.cs
+++ Editor/NeovimCodeEditor.cs
@@ -12,29 +12,42 @@ using Debug = UnityEngine.Debug;
 public class NeovimCodeEditor : IExternalCodeEditor
 {
     private static readonly string editorName = "Neovim (NvimUnity)";
-    private static readonly string launcherPath = "Packages/com.apyra.nvim-unity/Launch/nvim-open";
 
     static NeovimCodeEditor()
     {
         CodeEditor.Register(new NeovimCodeEditor());
-        EnsureProjectFiles();
+        //EnsureProjectFiles();
     }
 
-    public string GetDisplayName()
-    {
-        return editorName;
-    }
+    public string GetDisplayName() => editorName;
 
     public bool OpenProject(string path, int line, int column)
     {
+        if (string.IsNullOrEmpty(path))
+        {
+            Debug.LogError("[NvimUnity] OpenProject received empty path!");
+            return false;
+        }
+
         return OpenFileAtLine(path, line);
     }
 
     public bool OpenFileAtLine(string filePath, int line)
     {
+        if (string.IsNullOrEmpty(filePath))
+        {
+            Debug.LogError("[NvimUnity] filePath is null or empty!");
+            return false;
+        }
+
+        if (line < 1) line = 1;
+
         string fullPath = Path.GetFullPath(filePath);
-        string launchScript = GetLauncherPath();
-        Debug.Log(launchScript);
+        string launchScript = NormalizePath(GetLauncherPath());
+
+        Debug.Log($"[NvimUnity] Opening file: {fullPath} at line {line}");
+        Debug.Log($"[NvimUnity] Launching script: {launchScript}");
+        Debug.Log($"[NvimUnity] File exists? {File.Exists(launchScript)}");
 
         if (!File.Exists(launchScript))
         {
@@ -46,12 +59,17 @@ public class NeovimCodeEditor : IExternalCodeEditor
         {
             EnsureProjectFiles();
 
+	    Debug.Log($"[NvimUnity] Running command: cmd.exe /c \"\"{launchScript}\" \"{fullPath}\" +{line}\"");
+
+
             Process.Start(new ProcessStartInfo
-            {
-                FileName = launchScript,
-                Arguments = $"\"{fullPath}\" +{line}",
-                UseShellExecute = true
-            });
+{
+    FileName = "cmd.exe",
+    Arguments = $"/c \"\"{launchScript}\" \"{fullPath}\" +{line}\"",
+    CreateNoWindow = true,
+    UseShellExecute = false
+});
+
             return true;
         }
         catch (Exception e)
@@ -61,87 +79,79 @@ public class NeovimCodeEditor : IExternalCodeEditor
         }
     }
 
-    public void OnGUI()
-    {
-        // Optional: show settings in Preferences window
-    }
+    public void OnGUI() { }
 
     public void Initialize(string editorInstallationPath) { }
 
-    public CodeEditor.Installation[] Installations =>
-        new[]
+    public CodeEditor.Installation[] Installations => new[]
+    {
+        new CodeEditor.Installation
         {
-            new CodeEditor.Installation
+            Name = editorName,
+            Path = GetLauncherPath()
+        }
+    };
+
+    public void SyncAll()
+    {
+        AssetDatabase.Refresh();
+        SyncHelper.RegenerateProjectFiles();
+    }
+
+    public void SyncIfNeeded(string[] addedFiles, string[] deletedFiles, string[] movedFiles, string[] movedFromFiles, string[] importedFiles)
+    {
+        // Not implemented
+    }
+
+    public bool TryGetInstallationForPath(string editorPath, out CodeEditor.Installation installation)
+    {
+        var installations = Installations;
+
+        if (!installations.Any())
+        {
+            installation = new CodeEditor.Installation
             {
                 Name = editorName,
                 Path = GetLauncherPath()
-            }
-        };
-
-     // Unity calls this function during initialization in order to sync the Project. This is different from SyncIfNeeded in that it does not get a list of changes.
-  public void SyncAll()
-  {
-    AssetDatabase.Refresh();
-    SyncHelper.RegenerateProjectFiles();
-  }
-
-  // When you change Assets in Unity, this method for the current chosen instance of IExternalCodeEditor parses the new and changed Assets.
-  public void SyncIfNeeded(string[] addedFiles, string[] deletedFiles, string[] movedFiles, string[] movedFromFiles, string[] importedFiles)
-  {
-    /*(_projectGeneration.AssemblyNameProvider as IPackageInfoCache)?.ResetPackageInfoCache();*/
-    /*_projectGeneration.SyncIfNeeded(addedFiles.Union(deletedFiles).Union(movedFiles).Union(movedFromFiles).ToList(), importedFiles);*/
-    /**/
-    /**/
-  }
-
-
-    public bool TryGetInstallationForPath(string editorPath, out CodeEditor.Installation installation)
-  {
-    var lowerCasePath = editorPath.ToLower();
-    var filename = Path.GetFileName(lowerCasePath).Replace(" ", "");
-    var installations = Installations;
-
-    /*if (!_supportedFileNames.Contains(filename))*/
-    /*{*/
-    /*  installation = default;*/
-    /*  return false;*/
-    /*}*/
-
-    if (!installations.Any())
-    {
-      installation = new CodeEditor.Installation
-      {
-        Name = editorName,
-        Path =  GetLauncherPath()
-      };
-    }
-    else
-    {
-      try
-      {
-        installation = installations.First(inst => inst.Path == editorPath);
-      }
-      catch (InvalidOperationException)
-      {
-        installation = new CodeEditor.Installation
+            };
+        }
+        else
         {
-          Name = editorName,
-          Path = editorPath
-        };
-      }
-    }
+            try
+            {
+                installation = installations.First(inst => inst.Path == editorPath);
+            }
+            catch (InvalidOperationException)
+            {
+                installation = new CodeEditor.Installation
+                {
+                    Name = editorName,
+                    Path = editorPath
+                };
+            }
+        }
 
-    return true;
-  }
+        return true;
+    }
 
     private static string GetLauncherPath()
     {
+        string projectRoot = Directory.GetParent(Application.dataPath).FullName;
+        string scriptPath = Path.Combine(projectRoot, "Packages/com.apyra.nvim-unity/Launch/nvim-open");
+
 #if UNITY_EDITOR_WIN
-        return launcherPath + ".bat";
-#elif UNITY_EDITOR_OSX || UNITY_EDITOR_LINUX
-        return launcherPath + ".sh";
+        return scriptPath + ".bat";
 #else
-        return launcherPath;
+        return scriptPath + ".sh";
+#endif
+    }
+
+    private static string NormalizePath(string path)
+    {
+#if UNITY_EDITOR_WIN
+        return path.Replace("/", "\\");
+#else
+        return path.Replace("\\", "/");
 #endif
     }
 
@@ -156,7 +166,7 @@ public class NeovimCodeEditor : IExternalCodeEditor
 
         if (!hasCsproj || !hasSln)
         {
-            Debug.Log("Generating missing project files...");
+            Debug.Log("[NvimUnity] Generating missing project files...");
             SyncHelper.RegenerateProjectFiles();
         }
 
@@ -164,31 +174,8 @@ public class NeovimCodeEditor : IExternalCodeEditor
         if (!Directory.Exists(vscodePath))
         {
             Directory.CreateDirectory(vscodePath);
-            Debug.Log(".vscode folder created.");
+            Debug.Log("[NvimUnity] .vscode folder created.");
         }
     }
-
-    private static string GetPackagesFolderPath()
-    {
-        string assetsPath = Application.dataPath;
-        var parent = Directory.GetParent(assetsPath);
-        if (parent == null)
-        {
-            Debug.LogError("[nvim-unity] Could not resolve Packages folder.");
-            return null;
-        }
-        return Path.Combine(parent.FullName, "Packages");
-    }
-
-    private static string NormalizePath(string path)
-    {
-#if UNITY_EDITOR_WIN
-        return path.Replace("/", "\\");
-#else
-        return path.Replace("\\", "/");
-#endif
-    }
-
-
 }
 
