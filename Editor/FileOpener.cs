ce505193322f84caa3053e67e53065d0fd4dd128
apyra <apyra@outlook.com.br>
2025-04-09 11:25:03 -0300

    Added the socket checker class

--- Editor/FileOpener.cs
+++ Editor/FileOpener.cs
@@ -12,7 +12,6 @@ namespace NvimUnity
     {
         public static readonly string LauncherPath = Utils.GetLauncherPath();
         private static readonly string socket = Utils.GetSocketPath();
-        private static bool isProjectOpenInNeovim = SocketChecker.IsSocketActive(socket);
         
         public static bool OpenFile(string filePath, int line)
         {
@@ -22,7 +21,7 @@ namespace NvimUnity
             {
                 string normalizedPath = Utils.NormalizePath(filePath);
                 string root = Utils.FindProjectRoot(filePath);
-                string args = Utils.BuildLauncherCommand(filePath, line, socket, root, isProjectOpenInNeovim);
+                string args = Utils.BuildLauncherCommand(filePath, line, socket, root, SocketChecker.IsSocketActive(socket));
 
                 var psi = new ProcessStartInfo
                 {
@@ -39,20 +38,6 @@ namespace NvimUnity
             catch (Exception ex)
             {
                 Debug.LogWarning($"[NvimUnity] Could not start launcher: {ex.Message}");
-                return false;
-            }
-        }
-
-        bool IsSocketActive(string socketPath)
-        {
-            try
-            {
-                using var client = new NamedPipeClientStream(".", socketPath.Replace(@"\\.\pipe\", ""), PipeDirection.Out);
-                client.Connect(100); // tenta conectar em 100ms
-                return true;
-            }
-            catch
-            {
                 return false;
             }
         }
