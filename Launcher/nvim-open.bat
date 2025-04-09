443555eda2ccf3f536eb51a1ef384914eed11e5f
apyra <apyra@outlook.com.br>
2025-04-09 07:45:25 -0300

    Updated the .bat launcher

--- Launcher/nvim-open.bat
+++ Launcher/nvim-open.bat
@@ -14,29 +14,36 @@ for /f "tokens=2 delims=:" %%a in ('findstr /C:"Windows" "%CONFIG_FILE%"') do (
     set "TERMINAL=%%~a"
 )
 
-rem Lê socket
-for /f "tokens=2 delims=:" %%a in ('findstr /C:"socket" "%CONFIG_FILE%"') do (
-    set "SOCKET=%%~a"
-)
+REM rem Lê socket
+REM for /f "tokens=2 delims=:" %%a in ('findstr /C:"socket" "%CONFIG_FILE%"') do (
+REM     set "SOCKET=%%~a"
+REM )
 
 set TERMINAL=%TERMINAL:"=%  
 set TERMINAL=%TERMINAL:,=%  
 set TERMINAL=%TERMINAL: =%
 
-set SOCKET=%SOCKET:"=%
-set SOCKET=%SOCKET:,=%
-set SOCKET=%SOCKET: =%
-
-curl -s --max-time 1 %SERVER%status >nul
-if errorlevel 1 (
-    echo [NvimUnity] Server not found, opening with %TERMINAL%...
-    if /i "%TERMINAL%"=="wt" (
-        %TERMINAL% cmd /c "cd /d \"%ROOT%\" && nvim --listen %SOCKET% -c \"let g:unity_server = '%SERVER%'\" \"%FILE%\" +%LINE%"
-    ) else (
-        %TERMINAL% /c "cd /d \"%ROOT%\" && nvim --listen %SOCKET% -c \"let g:unity_server = '%SERVER%'\" \"%FILE%\" +%LINE%"
-    )
-    exit /b
+if /i "%TERMINAL%"=="wt" (
+   %TERMINAL% cmd /c "cd /d \"%ROOT%\" && nvim --remote-tab-silent -c \"let g:unity_server = '%SERVER%'\" \"%FILE%\" +%LINE%"
+) else (
+   %TERMINAL% /c "cd /d \"%ROOT%\" && nvim --remote-tab-silent -c \"let g:unity_server = '%SERVER%'\" \"%FILE%\" +%LINE%"
 )
 
-start "" curl -s -X POST %SERVER%open -H "Content-Type: text/plain" --data "%FILE%:%LINE%" >nul 2>&1
+
+REM set SOCKET=%SOCKET:"=%
+REM set SOCKET=%SOCKET:,=%
+REM set SOCKET=%SOCKET: =%
+
+REM curl -s --max-time 1 %SERVER%status >nul
+REM if errorlevel 1 (
+REM     echo [NvimUnity] Server not found, opening with %TERMINAL%...
+REM     if /i "%TERMINAL%"=="wt" (
+REM         %TERMINAL% cmd /c "cd /d \"%ROOT%\" && nvim --listen %SOCKET% -c \"let g:unity_server = '%SERVER%'\" \"%FILE%\" +%LINE%"
+REM     ) else (
+REM         %TERMINAL% /c "cd /d \"%ROOT%\" && nvim --listen %SOCKET% -c \"let g:unity_server = '%SERVER%'\" \"%FILE%\" +%LINE%"
+REM     )
+REM     exit /b
+REM )
+
+REM start "" curl -s -X POST %SERVER%open -H "Content-Type: text/plain" --data "%FILE%:%LINE%" >nul 2>&1
 
